cea5581dad12203e7ed68292fc9a0a0eb4e75fc1
Alireza Yousefi <aryadesignco@gmail.com>
2021-10-27 15:38:16 +0330

    Example Model Added

---
+++ app/Models/Example.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model;
+
+class Example extends Model
+{
+    protected static $table = 'table';
+
+    protected static $fillable = [
+        'column',
+    ];
+
+    public static function get()
+    {
+        $tb = self::$table;
+        return parent::all($tb);
+    }
+
+    public static function find($id)
+    {
+        $tb = self::$table;
+        return parent::single($tb, $id);
+    }
+
+    public static function findOrFail($id)
+    {
+        $tb = self::$table;
+        return parent::singleFail($tb, $id);
+    }
+
+    public static function create($data)
+    {
+        $tb = self::$table;
+        $fb = self::$fillable;
+        return parent::insert($tb, $data, $fb);
+    }
+
+    public static function update($id, $data)
+    {
+        $tb = self::$table;
+        $fb = self::$fillable;
+        return parent::edit($tb, $data, $fb, $id);
+    }
+}

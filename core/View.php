126a523f1a2d117f253c93551b1605b4c81968f4
Alireza Yousefi <aryadesignco@gmail.com>
2021-11-11 10:49:13 +0330

    View Core Updated

--- core/View.php
+++ core/View.php
@@ -22,38 +22,38 @@ class View
         return $url;
     }
 
+    public static function functions()
+    {
+        $opts = [];
+        $opts['route'] = new TwigFunction('route', function($name, $params = null) {
+            return Route::url($name, $params);
+        });
+        $opts['redirect'] = new TwigFunction('redirect', function($name) {
+            return Route::redirect($name);
+        });
+        $opts['asset'] = new TwigFunction('asset', function($file) {
+            return self::getUrl() . "assets/" . $file;
+        });
+        $opts['session'] = new TwigFunction('session', function($name) {
+            return isset($_COOKIE[$name]) ? $_COOKIE[$name] : false;
+        });
+        $opts['auth'] = new TwigFunction('auth', function() {
+            return Auth::check();
+        });
+        $opts['user'] = new TwigFunction('user', function() {
+            return Auth::user();
+        });
+        $opts['count'] = new TwigFunction('count', function($arr) {
+            return count($arr);
+        });
+        return $opts;
+    }
+
     public static function render($file, $args = [])
     {
         $loader = new FilesystemLoader(__DIR__ . "/../resources/views");
         $twig = new Environment($loader);
-        $route = new TwigFunction('route', function($name, $params = null) {
-            return Route::url($name, $params);
-        });
-        $twig->addFunction($route);
-        $redirect = new TwigFunction('redirect', function($name) {
-            return Route::redirect($name);
-        });
-        $twig->addFunction($redirect);
-        $asset = new TwigFunction('asset', function($file) {
-            return self::getUrl() . "assets/" . $file;
-        });
-        $twig->addFunction($asset);
-        $session = new TwigFunction('session', function($name) {
-            return isset($_COOKIE[$name]) ? $_COOKIE[$name] : false;
-        });
-        $twig->addFunction($session);
-        $auth = new TwigFunction('auth', function() {
-            return Auth::check();
-        });
-        $twig->addFunction($auth);
-        $user = new TwigFunction('user', function() {
-            return Auth::user();
-        });
-        $twig->addFunction($user);
-        $count = new TwigFunction('count', function($arr) {
-            return count($arr);
-        });
-        $twig->addFunction($count);
+        foreach (self::functions() as $item) $twig->addFunction($item);
         echo $twig->render("$file.twig", $args);
     }
 }
